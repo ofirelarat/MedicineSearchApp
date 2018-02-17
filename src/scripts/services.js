0d4c71257e9284088246386fb3137ed001652b0f
ofirelarat <96ofir11@gmail.com>
2018-02-17 14:40:50 +0200

    add(service): add get by filter function

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,9 +1,24 @@
 var app = require("./app.js");
 
-app.service("medicinesFactory",["$http", function($http) {
-    var urlBase = "http://medicine-cabinet-api.azurewebsites.net/medicine/all";
+app.service("medicinesFactory", [
+    "$http",
+    function($http) {
+        var urlBase = "http://medicine-cabinet-api.azurewebsites.net/medicine/";
 
-    this.getMedicines = function(limit,offset){
-        return $http.get(urlBase + "?limit="+limit + "&offset="+offset);
+        this.getMedicines = function(limit, offset) {
+            return $http.get(
+                urlBase + "all?limit=" + limit + "&offset=" + offset
+            );
+        };
+
+        this.getFilterMedicines = function(filterValue, filterName) {
+            return $http.get(
+                urlBase +
+                "getByFilter?filterValue=" +
+                filterValue +
+                "&filterName=" +
+                filterName
+            );
+        };
     }
-}]);
+]);
